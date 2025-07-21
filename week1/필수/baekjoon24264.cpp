c09376ec6cb0d6209d74c1021da1005933ae2aab
Jeewon Koo <jeewon514@gmail.com>
2025-07-21 14:53:24 +0900

    알고리즘수업24264(Bronze3)

--- week1/필수/baekjoon24264.cpp
+++ week1/필수/baekjoon24264.cpp
@@ -6,6 +6,7 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    // n의 범위가 500,000까지 가능하니까 int는 25,000,000,000 이라서 범위를 초과함.
     long long n;
     cin>>n;
     cout<<n*n<<'\n'<<2<<'\n';
