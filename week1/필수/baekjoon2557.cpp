accb7f106f1874b5fc6051a6b9712e8d057b7453
Jeewon Koo <jeewon514@gmail.com>
2025-07-21 14:49:07 +0900

    HelloWorld2557(Bronze5)

---
+++ week1/필수/baekjoon2557.cpp
@@ -0,0 +1,11 @@
+// Hello World - 2557(Bronze5)
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
