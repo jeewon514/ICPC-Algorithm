1f5fd1391e5d8ada440edb3930c6e5f1800509fb
Jeewon Koo <jeewon514@gmail.com>
2025-07-21 14:50:15 +0900

    사직연산10869(Bronze5)

---
+++ week1/필수/baekjoon10869.cpp
@@ -0,0 +1,13 @@
+//사직연산 - 10869(Bronze5)
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int A,B;
+    cin>>A>>B;
+    cout<<A+B<<endl<<A-B<<endl<<A*B<<endl<<A/B<<endl<<A%B<<endl;
+    return 0;
+}
