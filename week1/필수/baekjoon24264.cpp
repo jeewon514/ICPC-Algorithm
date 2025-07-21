af669ae5f4e83353a4640c40a39c61755bf286f8
Jeewon Koo <jeewon514@gmail.com>
2025-07-21 14:51:40 +0900

    알고리즘수업24264(Bronze3)

---
+++ week1/필수/baekjoon24264.cpp
@@ -0,0 +1,16 @@
+//알고리즘 수업 - 24264(Bronze3)
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    cin>>n;
+    cout<<n*n<<'\n'<<2<<'\n';
+
+    return 0;
+}
+
+
